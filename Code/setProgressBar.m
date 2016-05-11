87f58c44b42e87df2644c0b05433c2b4b5d2aecb
dansaunders200713 <dansaunders200713@gmail.com>
2016-05-11 14:17:13 -0400

    Initial commit. Code written by a previous COGNAC employing; repurposing for a new study.

---
+++ Code/setProgressBar.m
@@ -0,0 +1,10 @@
+function setProgressBar(pBar,status)
+pPatch = getappdata(pBar,'pPatch');
+if status <=0
+    set(pPatch,'visible','off');
+else
+%elseif status <= 14
+    set(pPatch,'visible','on');
+    set(pPatch, 'YData', [0 0 status status])
+end
+drawnow;
